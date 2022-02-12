9c446707ee514deb9802742b977ff1aa8451609e
Tim <timofeyobraztsov@gmail.com>
2022-02-12 11:23:24 -0800

    refer to mint website

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const model = document.querySelector('model-viewer');
 model.addEventListener('model-visibility', () => {
 	model.style.visibility = 'visible';
 });
+
+const button = document.querySelector('.connectButton');
+button.addEventListener('click', () => {
+	window.location = 'https://mint.mintmachines.xyz/';
+});
